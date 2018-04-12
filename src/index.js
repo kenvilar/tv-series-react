e1bbb9eb2a056eb5fc89eb7dbcdc6ea3a432486f
Ken Vilar <kenvilar@gmail.com>
2018-04-12 23:14:23 +0800

    Use jsx

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const greeting = React.createElement('h1', {}, 'This is react app!');
+const getCurrentDate = () => {
+	const date = new Date();
+
+	return date.toDateString();
+};
+const greeting = <h1>This is a react app. <br/>Current Date: {getCurrentDate()}</h1>
 ReactDOM.render(greeting, document.getElementById('root'));
 //ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
