d9c0a7162607bd1c4d098969af92a98b13905981
Ken Vilar <kenvilar@gmail.com>
2018-04-13 13:06:22 +0800

    Refactor List component

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './index.css';
 
+const SeriesListItem = ({series}) => (
+	<li key={series.show.id}>{series.show.name}</li>
+);
+
 const SeriesList = (props) => {
 	return (
 		<div>
 			<ul className="series-list-group">
 				{props.list.map(series => (
-					<li key={series.show.id}>{series.show.name}</li>
+					<SeriesListItem series={series}/>
 				))}
 			</ul>
 		</div>
