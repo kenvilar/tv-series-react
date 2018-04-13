bb7ceafa73feb6ab5e7c2e17f527e9a4ba20d695
Ken Vilar <kenvilar@gmail.com>
2018-04-13 15:21:27 +0800

    Remove css

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import loaderSrc from '../../assets/loader.gif';
-import './Loader.css';
 
 const Loader = (props) => (
 	<div>
-		<img className="loader" src={loaderSrc} alt="loader-image" title="loader-image"/>
+		<img style={{width: 200}} className="loader" src={loaderSrc} alt="loader-image" title="loader-image"/>
 	</div>
 );
 
