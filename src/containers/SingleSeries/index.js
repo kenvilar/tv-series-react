03bb099206d8b8bf4e90fd4ee4572f36d1ec9cc5
Ken Vilar <kenvilar@gmail.com>
2018-04-13 16:02:35 +0800

    Route params of a single series

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
+import Loader from '../../components/Loader';
 
 class SingleSeries extends Component {
+	state = {
+		show: null
+	};
+
+	componentDidMount() {
+		const {id} = this.props.match.params;
+		fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+			.then((response) => {
+				return response.json();
+			})
+			.then((json) => {
+				this.setState({show: json});
+			})
+			.catch((ex) => {
+				console.log('parsing failed', ex);
+			});
+	}
+
 	render() {
+		console.log(this.props);
+		const {show} = this.state;
+		console.log(show);
 		return (
 			<div>
-				<p>Single Series - The show id will be {this.props.match.params.id}</p>
+				{
+					show === null
+					&&
+					<Loader/>
+				}
 			</div>
 		);
 	}
